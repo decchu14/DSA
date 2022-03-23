37ecc7b9639a5bde698f5aed021309dfebe3e668
decchu14 <1decchu14@gmail.com>
2022-03-24 01:20:02 +0530

    Adding the program to count total number of setbits in a and b

---
+++ 8_setbit.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int setbit(int a, int b)
+{
+    int count = 0;
+    while (a > 0)
+    {
+        if (a & 1)
+            count++;
+        a = a >> 1;
+    }
+    while (b > 0)
+    {
+        if (b & 1)
+            count++;
+        b = b >> 1;
+    }
+
+    return count;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << setbit(a, b);
+    return 0;
+}
